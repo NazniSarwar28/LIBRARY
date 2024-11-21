ce65b3456d2f8d20a13d6ef72284a5e1553d1c03
Nazni Sarwar <naznisarwar4321@gmail.com>
2024-11-21 08:12:17 +0530

    Add files via upload

---
+++ LibraryManagementSystem/build/generated/src/org/apache/jsp/editBook_jsp.java
@@ -0,0 +1,97 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class editBook_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html; charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("<head>\n");
+      out.write("    <meta charset=\"UTF-8\">\n");
+      out.write("    <title>Library Management System- Edit Book</title>\n");
+      out.write("</head>\n");
+      out.write("<body>\n");
+      out.write("<h2>Edit Book Details</h2>\n");
+      out.write("\n");
+ 
+    String bookId = request.getParameter("bookId");
+    String bookName = request.getParameter("bookName");
+    String authorName = request.getParameter("authorName");
+    String category = request.getParameter("category");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("<form action=\"Book\" method=\"POST\">\n");
+      out.write("    <label>Book ID: <input type=\"text\" name=\"bookId\" value=\"");
+      out.print( bookId );
+      out.write("\" readonly></label><br><br>\n");
+      out.write("    <label>Book Name: <input type=\"text\" name=\"bookName\" value=\"");
+      out.print( bookName );
+      out.write("\" required></label><br><br>\n");
+      out.write("    <label>Author Name: <input type=\"text\" name=\"authorName\" value=\"");
+      out.print( authorName );
+      out.write("\" required></label><br><br>\n");
+      out.write("    <label>Category: <input type=\"text\" name=\"category\" value=\"");
+      out.print( category );
+      out.write("\" required></label><br><br>\n");
+      out.write("    <input type=\"hidden\" name=\"action\" value=\"edit\">\n");
+      out.write("    <input type=\"submit\" value=\"Update Book\">\n");
+      out.write("</form>\n");
+      out.write("<br/>    \n");
+      out.write("<a href=\"books.jsp\">Back to Book List</a>\n");
+      out.write("\n");
+      out.write("</body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
